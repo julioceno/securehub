489a42857f9b50feb1855b5a418006a03a5e1c6d
julioceno <julioreplay@gmail.com>
2025-11-23 22:50:44 -0300

    feat: (mailsender) Enhance EmailConsumer with retry logic and error logging; update KafkaConsumerConfig for producer and task scheduler

--- mailsender/src/main/java/com/securehub/mailsender/adapter/in/EmailConsumer.java
+++ mailsender/src/main/java/com/securehub/mailsender/adapter/in/EmailConsumer.java
@@ -4,8 +4,11 @@ import com.securehub.mailsender.application.usecases.SendMailUseCase;
 import com.securehub.mailsender.application.util.CorrelationId;
 import com.securehub.mailsender.domain.EmailMessage;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,18 @@ import tools.jackson.databind.ObjectMapper;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EmailConsumer {
     private final ObjectMapper objectMapper;
     private final SendMailUseCase sendMailUseCase;
 
+    @RetryableTopic(
+            attempts = "3",
+            backOff = @BackOff(delay = 2000, multiplier = 2.0),
+            dltTopicSuffix = "-dlt",
+            autoCreateTopics = "true",
+            include = {Exception.class}
+    )
     @KafkaListener(
             topics = "${kafka.consumer.email.topic}",
             groupId = "${kafka.consumer.group-id}",
@@ -25,11 +36,15 @@ public class EmailConsumer {
     public void listen(
             @Payload String emailMessage,
             @Header(CorrelationId.HEADER_NAME) String correlationId
-    ) {
+        ) {
         try {
             MDC.put(CorrelationId.HEADER_NAME, correlationId);
+
             EmailMessage email = objectMapper.readValue(emailMessage, EmailMessage.class);
             sendMailUseCase.run(email);
+        } catch (Exception e) {
+            log.error("EmailConsumer.listen - Error - correlationId [{}] - error:", correlationId, e);
+            throw e;
         } finally {
             MDC.clear();
         }
